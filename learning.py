0037c38373cb26727c0c8063c0f994fc48d3d2cf
jiamingchao26 <jiamingchao26@gmail.com>
2025-11-02 21:03:41 +0800

    first_commit

---
+++ learning.py
@@ -0,0 +1,9 @@
+# ab_plus_cd.py
+import torch
+def ab_plus_cd(a, b, c, d):
+    return a * b + c * d
+
+if __name__ == "__main__":
+    # 方式一：手动修改数值
+    a, b, c, d = 2, 3, 4, 5
+    print(ab_plus_cd(a, b, c, d))
